74b5dbc7d61400da5bc0b23125e3d3f86cd13bda
System Administrator <root@Kevins-Old-MacBook-Pro.local>
2021-01-31 19:28:09 -0500

    last second changes

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ module.exports.addUser = function(newUser, callback){
     bcrypt.hash(newUser.password, salt, (err, hash) => {
       if(err) throw err;
       newUser.password = hash;
-      newUser.save(callback);
+      newUser.save();
     })
   })
 }
--- practical/src/App.js
+++ practical/src/App.js
@@ -12,6 +12,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import GuardedRoute from './components/Guard';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -37,7 +38,7 @@ function App() {
         <Toolbar>
 
           <Typography variant="h6" className={classes.title}>
-            DAMBLR: The Official DAML Blog
+            <Button color="inherit" href="/">DAMBLR: The Official DAML Blog</Button>
           </Typography>
           {isLoggedIn ? <Button color="inherit" href="/" onClick={logout}>Logout</Button>:<Button color="inherit" href="/login">Login</Button>}
 
@@ -46,7 +47,7 @@ function App() {
         <Switch>
           <Route path="/login" exact component={Login} />
           <Route path="/register" exact component={Register} />
-          <Route path="/dashboard" exact component={Dashboard} />
+          <GuardedRoute path="/dashboard" exact component={Dashboard} auth={isLoggedIn}/>
         </Switch>
       </Router>
     </div>
